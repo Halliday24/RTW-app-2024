7fe1622b22a60cf12fc5f430c7ddbc715312d329
the-mayowa <davidmayowa352@gmail.com>
2024-01-08 15:33:14 -0700

    peer programming with Emmanuel. He started impact Page which will be our first page after login.

---
+++ app/src/main/java/com/example/rtw_app/InstructionPage.java
@@ -0,0 +1,14 @@
+package com.example.rtw_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstructionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction_page);
+    }
+}
---
+++ app/src/main/java/com/example/rtw_app/impactPage.java
@@ -0,0 +1,16 @@
+package com.example.rtw_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class impactPage extends AppCompatActivity {
+
+    //this page is the java file for the xml of the section that asks about impact
+    // on the students
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_impact_page);
+    }
+}
