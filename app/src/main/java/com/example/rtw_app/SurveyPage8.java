942a765200c36c8c2b67908a2fa7a2cc744d4372
Halliday24 <bomahalliday92@gmail.com>
2024-01-15 16:08:24 -0700

    Created SurveyPage8.java and activity_survey_page8.xml

---
+++ app/src/main/java/com/example/rtw_app/SurveyPage8.java
@@ -0,0 +1,86 @@
+package com.example.rtw_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class SurveyPage8 extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_page8);
+        sharedPreferences = getSharedPreferences("survey_responses", MODE_PRIVATE);
+
+        TextView textView = (TextView) findViewById(R.id.surveyPage8_Question);
+        textView.setText("Please describe any other barriers, including any extenuating circumstances, you " +
+                "may have encountered. For example: family emergencies or child care issues. ");
+
+        TextView textView2 = (TextView) findViewById(R.id.surveyPage8_Question2);
+        textView2.setText("What are the primary sources of stress in your life?");
+
+
+
+
+        Button buttonNext=findViewById(R.id.nextButton);
+
+        //set a click listener for the next Button
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                goToSurveyPage9();
+            }
+        });
+        Button buttonBack=findViewById(R.id.BackButton);
+
+        //set a click listener for the next Button
+        buttonBack.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                goBack();
+            }
+        });
+
+        // Set text color for all TextViews in the layout
+        setTextColorForAllTextViews((ViewGroup) findViewById(android.R.id.content), Color.BLACK);
+    }
+
+    private void setTextColorForAllTextViews(ViewGroup viewGroup, int color) {
+        int childCount = viewGroup.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View childView = viewGroup.getChildAt(i);
+            if (childView instanceof TextView) {
+                // Check if the view is a TextView
+                TextView textView = (TextView) childView;
+                textView.setTextColor(color);
+            } else if (childView instanceof ViewGroup) {
+                // If the view is a ViewGroup, recursively call the method
+                setTextColorForAllTextViews((ViewGroup) childView, color);
+            }
+        }
+
+    }
+
+    public void goToSurveyPage9(){
+        Intent SurveyPage9 = new Intent(this, SurveyPage9.class);
+        startActivity(SurveyPage9);
+
+    }
+
+    public void goBack(){
+        Intent impactAcademicPage2 = new Intent(this, ImpactFinancial.class);
+        startActivity(impactAcademicPage2);
+
+    }
+}
