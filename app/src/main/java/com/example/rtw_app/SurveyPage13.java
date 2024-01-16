2d17ca10409407b99fc1c4340368ee478a378b6c
the-mayowa <davidmayowa352@gmail.com>
2024-01-16 10:10:03 -0700

    Modifications on these few pages

--- app/src/main/java/com/example/rtw_app/SurveyPage13.java
+++ app/src/main/java/com/example/rtw_app/SurveyPage13.java
@@ -2,6 +2,7 @@ package com.example.rtw_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SurveyPage13 extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class SurveyPage13 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_page13);
+    }
+
+    public void goToSurveyPage14() {
+        Intent SurveyPage14 = new Intent(this, SurveyPage14.class);
+        startActivity(SurveyPage14);
     }
 }
