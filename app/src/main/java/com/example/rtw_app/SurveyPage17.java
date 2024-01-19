1ebc15de3d2c5c3b066a1f775fe47f4bdeb41897
LegoLiamNeeson <ctiede81@gmail.com>
2024-01-19 10:28:51 -0700

    Added the pdf generator to Question 17, 18,19 ,20 parts one and two

--- app/src/main/java/com/example/rtw_app/SurveyPage17.java
+++ app/src/main/java/com/example/rtw_app/SurveyPage17.java
@@ -3,37 +3,108 @@ package com.example.rtw_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SurveyPage17 extends AppCompatActivity {
 
     private Button hint;
+    private String userInfo;
+
+
+    private SharedPreferences sharedPreferences;
+    private RadioGroup colorRadioGroup, timeRadioGroup, poorStudyRadioGroup, disabilityRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_page17);
+        userInfo = getIntent().getStringExtra("userInfo");
 
         hint = findViewById(R.id.hint);
         //Set an onClick listener for using the hint button
+
+        // Initialize your RadioGroup instances
+        colorRadioGroup = findViewById(R.id.colorRadioGroup);
+        timeRadioGroup = findViewById(R.id.timeRadioGroup);
+        poorStudyRadioGroup = findViewById(R.id.poorStudyRadioGroup2);
+        disabilityRadioGroup = findViewById(R.id.disabilityRadioGroup);
         hint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openHint();
             }
         });
+
+        Button submitButton = findViewById(R.id.nextButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Call the generateAndSavePdf method
+                generateAndSavePdf();
+            }
+        });
+    }
+    private void generateAndSavePdf() {
+        List<String> questionTexts = new ArrayList<>();
+        String mainQuestion = "Reading and Writing Skills";
+
+        // Add your question texts to the list here
+        questionTexts.add("My long papers have multiple\n" +
+                "drafts ");
+        questionTexts.add("I feel confident about my\n" +
+                "writing ability");
+        questionTexts.add("Second Additional Question");
+        questionTexts.add("Second Additional Question");
+        String output = userInfo + "_survey_output28.pdf";
+
+        // Call the PdfGenerator to generate PDF
+        PdfGenerator.generatePdf(SurveyPage17.this, output,
+                getSurveyAnswers(), questionTexts, mainQuestion);
+
+        // Call the goTo method after generating and saving the PDF
+        goTo();
     }
 
+    private List<String[]> getSurveyAnswers() {
+        List<String[]> answersList = new ArrayList<>();
+
+        // Create an array with the survey answers and add it to the list
+        String[] surveyAnswers = {
+                getSelectedRadioButtonText(colorRadioGroup),
+                getSelectedRadioButtonText(timeRadioGroup),
+                getSelectedRadioButtonText(poorStudyRadioGroup),
+                getSelectedRadioButtonText(disabilityRadioGroup)
+        };
+        answersList.add(surveyAnswers);
+
+        return answersList;
+    }
+
+    private String getSelectedRadioButtonText(RadioGroup radioGroup) {
+        int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+        if (selectedRadioButtonId != -1) {
+            return ((RadioButton)findViewById(selectedRadioButtonId)).getText().toString();
+        }
+        return "";
+    }
     public void goTo(){
         Intent SurveyPage18 = new Intent(this, SurveyPage18.class);
+        SurveyPage18.putExtra("userInfo", userInfo);
         startActivity(SurveyPage18);
 
     }
 
     public void goBack(){
         Intent SurveyPage16p2 = new Intent(this, SurveyPage16p2.class);
+        SurveyPage16p2.putExtra("userInfo", userInfo);
         startActivity(SurveyPage16p2);
 
     }
--- app/src/main/java/com/example/rtw_app/SurveyPage20p1.java
+++ app/src/main/java/com/example/rtw_app/SurveyPage20p1.java
@@ -3,20 +3,37 @@ package com.example.rtw_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SurveyPage20p1 extends AppCompatActivity {
+    private String userInfo;
 
+
+    private SharedPreferences sharedPreferences;
+    private RadioGroup colorRadioGroup, timeRadioGroup, poorStudyRadioGroup;
     private Button hint;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_page20p1);
-
+        sharedPreferences = getSharedPreferences("survey_responses", MODE_PRIVATE);
+        userInfo = getIntent().getStringExtra("userInfo");
         hint = findViewById(R.id.hint);
+
+        // Initialize your RadioGroup instances
+        colorRadioGroup = findViewById(R.id.colorRadioGroup);
+        timeRadioGroup = findViewById(R.id.timeRadioGroup);
+        poorStudyRadioGroup = findViewById(R.id.poorStudyRadioGroup2);
         //Set an onClick listener for using the hint button
         hint.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,16 +41,71 @@ public class SurveyPage20p1 extends AppCompatActivity {
                 openHint();
             }
         });
+
+        Button submitButton = findViewById(R.id.nextButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Call the generateAndSavePdf method
+                generateAndSavePdf();
+            }
+        });
     }
+
+    private void generateAndSavePdf() {
+        List<String> questionTexts = new ArrayList<>();
+        String mainQuestion = "Stress and Wellbeing";
+
+        // Add your question texts to the list here
+        questionTexts.add("I believe in my capacity to do\n" +
+                "well in university ");
+        questionTexts.add("I ask for help when I need it ");
+        questionTexts.add("I have little difficulty managing\n" +
+                "challenges in my life ");
+
+        String output = userInfo + "_output31.pdf";
+
+        // Call the PdfGenerator to generate PDF
+        PdfGenerator.generatePdf(SurveyPage20p1.this, output,
+                getSurveyAnswers(), questionTexts, mainQuestion);
+
+        // Call the goTo method after generating and saving the PDF
+        goTo();
+    }
+
+    private List<String[]> getSurveyAnswers() {
+        List<String[]> answersList = new ArrayList<>();
+
+        // Create an array with the survey answers and add it to the list
+        String[] surveyAnswers = {
+                getSelectedRadioButtonText(colorRadioGroup),
+                getSelectedRadioButtonText(timeRadioGroup),
+                getSelectedRadioButtonText(poorStudyRadioGroup)
+        };
+        answersList.add(surveyAnswers);
+
+        return answersList;
+    }
+
+    private String getSelectedRadioButtonText(RadioGroup radioGroup) {
+        int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+        if (selectedRadioButtonId != -1) {
+            return ((RadioButton) findViewById(selectedRadioButtonId)).getText().toString();
+        }
+        return "";
+    }
+
 
     public void goTo(){
         Intent SurveyPage20p2 = new Intent(this, SurveyPage20p2.class);
+        SurveyPage20p2.putExtra("userInfo", userInfo);
         startActivity(SurveyPage20p2);
 
     }
 
     public void goBack(){
         Intent SurveyPage19 = new Intent(this, SurveyPage19.class);
+        SurveyPage19.putExtra("userInfo", userInfo);
         startActivity(SurveyPage19);
 
     }
